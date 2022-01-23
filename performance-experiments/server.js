0813b2db6084b48085e2abd6279a8fa39b0150c2
Ashish Karki <ashishk@Ashishs-MacBook-Pro.local>
2022-01-23 17:57:13 +0545

    feat: 'performance-experiments' project - using pm2 as package.json scripts

--- performance-experiments/server.js
+++ performance-experiments/server.js
@@ -1,4 +1,6 @@
 const express = require('express')
+// const cluster = require('cluster')
+// const os = require('os')
 
 const app = express()
 
@@ -21,14 +23,30 @@ app.get('/', (req, res) => {
 
   // another example is the operations in the node/crypto module
 
-  res.send('Hello World of Performance!')
+  res.send('Hello World of Performance! with proces id: ${process.pid}')
 })
 
 app.get('/timers', (req, res) => {
   delay(9000)
-  res.send(`Tring Tring..Timer/s done`)
+  res.send(`Tring Tring..Timer/s done with process id: ${process.pid}`)
 })
 
+// if using pm2, clustering is built in, so no need to check isMaster etc
+// if (cluster.isMaster) {
+//   console.log(`Master ${process.pid} is running`)
+
+//   const NUM_LOGICAL_CPUS = os.cpus().length
+
+//   for (let i = 0; i < NUM_LOGICAL_CPUS; i++) {
+//     cluster.fork()
+//   }
+//   //   cluster.fork()
+//   //   cluster.fork()
+// } else {
+// const workerId = cluster.worker.id
+// console.log(`Worker ${process.pid} is running`)
+
 app.listen(3000, () => {
-  console.log('Listening on port 3000')
+  console.log('Worker  listening on port 3000')
 })
+// }
